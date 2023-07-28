707274621b1f5ef3be3a0ab06688781311983479
clxdev <domhenin.517@gmail.com>
2023-07-28 16:19:48 -0400

    still wip :: worked on sgs for ws && db

--- modules/aws/network-base/main.tf
+++ modules/aws/network-base/main.tf
@@ -30,3 +30,51 @@ resource "aws_subnet" "private_subnet" {
     }
 }
 
+
+resource "aws_internet_gateway" "igw" {
+    vpc_id = aws_vpc.vpc.id
+
+    tags = {
+        Name = var.igw_tag
+    }
+}
+
+resource "aws_security_group" "sg_apache" {
+    vpc_id = aws_vpc.vpc.id
+    name = join("_", ["webserver-sg", aws_vpc.vpc.id])
+    dynamic "ingress" {
+        for_each = var.public_rules
+        content {
+            from_port = ingress.value["port"]
+            to_port = ingress.value["port"]
+            protocol = ingress.value["porto"]
+            cidr_blocks = ingress.value["cidr_blocks"]
+        }
+    }
+    egress {
+        from_port = 0
+        to_port = 0
+        protocol = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    tags = {
+        Name = var.sg_apache_tag
+    }
+}
+
+resource "aws_security_group" "sg_rds" {
+    vpc_id = aws_vpc.vpc.id
+    name = join("_", ["database-sg", aws_vpc.vpc.id])
+    ingress {
+        description = "Allow traffic"
+        from_port = 3306
+        to_port = 3306
+        protocol = "tcp"
+        # cidr_blocks = ["10.0.0.0/24"] # replace this with the actual source IP range of your application servers
+    }
+
+    tags = {
+        Name = var.sg_rds_tag
+    }
+}
--- modules/aws/network-base/variables.tf
+++ modules/aws/network-base/variables.tf
@@ -43,3 +43,47 @@ variable "priv_sub_tag" {
     default = "private_subnet"
 }
 
+
+# INTERNET GATEWAY
+variable "igw_tag" {
+    description = "internet gateway tag"
+    type = string
+    default = "igw_tag"
+}
+
+variable "public_rules" {
+    type = list(object({
+        port = number
+        porto = string
+        cidr_blocks = list(string)
+    }))
+    default = [
+        {
+            port = 80
+            porto = "tcp"
+            cidr_blocks = ["0.0.0.0/0"]
+        },
+        {
+            port = 22
+            porto = "tcp"
+            cidr_blocks = ["0.0.0.0/0"]
+        }
+        # {
+        #     port = 3689
+        #     porto = "tcp"
+        #     cidr_blocks = [""]
+        # }
+    ]
+}
+
+variable "sg_apache_tag" {
+    description = "security group tag for WebServer"
+    type = string
+    default = "web-server_security_group"
+}
+
+variable "sg_rds_tag" {
+    description = "security group for RDS Database"
+    type = string
+    default = "rds_security_group"
+}
