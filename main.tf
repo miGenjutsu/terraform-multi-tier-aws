0f950fa7955261b3558904e01e4d73386f0e456a
migenjutsu <domhenin.517@gmail.com>
2023-07-28 23:00:53 -0400

    still wip

--- main.tf
+++ main.tf
@@ -5,9 +5,15 @@
 
 
 provider "aws" {
-    region = var.aws_region
+  region = var.aws_region
 }
 
 module "network-base" {
-    source = "./modules/aws/network-base/"
+  source = "./modules/aws/network-base/"
+}
+
+module "compute-base" {
+  source = "./modules/aws/compute-base/"
+  public_subnet_id = module.network-base.public_subnet_output
+  security_group_webserver_id = module.network-base.sg_webserver_output
 }
