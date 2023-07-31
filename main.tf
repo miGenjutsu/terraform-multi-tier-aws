95607278315d554f10c2f19137a417423416af46
migenjutsu <domhenin.517@gmail.com>
2023-07-31 09:33:21 -0400

    still wip :: will resolve tfvars issue after build

--- main.tf
+++ main.tf
@@ -10,10 +10,25 @@ provider "aws" {
 
 module "network-base" {
   source = "./modules/aws/network-base/"
+  cidr_range = var.cidr_range
+  vpc_tag = var.vpc_tag
+
+  pub_sub_cidr = var.pub_sub_cidr
+  pub_sub_tag = var.pub_sub_tag
+
+  priv_sub_cidr = var.priv_sub_cidr
+  priv_sub_tag = var.priv_sub_tag
+
+  igw_tag = var.igw_tag
+
+  sg_apache_tag = var.sg_apache_tag
+  sg_rds_tag = var.sg_rds_tag
+
+  rtb_tag = var.rtb_tag
 }
 
-module "compute-base" {
-  source = "./modules/aws/compute-base/"
-  public_subnet_id = module.network-base.public_subnet_output
-  security_group_webserver_id = module.network-base.sg_webserver_output
-}
+# module "compute-base" {
+#   source = "./modules/aws/compute-base/"
+#   public_subnet_id = module.network-base.public_subnet_output
+#   security_group_webserver_id = module.network-base.sg_webserver_output
+# }
--- modules/aws/network-base/main.tf
+++ modules/aws/network-base/main.tf
@@ -78,3 +78,34 @@ resource "aws_security_group" "sg_rds" {
   #     Name = var.sg_rds_tag
   # }
 }
+
+
+
+# ---- Route Table
+resource "aws_route_table" "rt_rtb" {
+  vpc_id = aws_vpc.vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
+  # route {
+  #   ipv6_cidr_block        = "::/0"
+  #   egress_only_gateway_id = aws_egress_only_internet_gateway.example.id
+  # }
+
+  tags = {
+    Name = var.rtb_tag
+  }
+}
+
+resource "aws_route_table_association" "rt_assoc_public" {
+  subnet_id = aws_subnet.public_subnet.id
+  route_table_id = aws_route_table.rt_rtb.id
+}
+
+# resource "aws_route_table_association" "rt_assoc_private" {
+#   subnet_id = aws_subnet.private_subnet.id
+#   route_table_id = aws_route_table.rt_rtb.id
+# }
